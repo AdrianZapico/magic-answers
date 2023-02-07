4f7c58bc910ed209c0f92ce9da6ee9ea3661e67f
Atila Zapico <zapicoatila00@gmail.com>
2023-02-07 20:15:57 -0300

    new test

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 const inputQuestion = document.getElementById("inputQuestion");
 const result = document.getElementById("result");
 
@@ -7,7 +6,7 @@ inputQuestion.addEventListener("keypress", (e) => {
   if (inputQuestion.value && e.key === "Enter") SendQuestion();
 });
 
-const OPENAI_API_KEY = API_KEY
+const OPENAI_API_KEY = "";
 
 function SendQuestion() {
   var sQuestion = inputQuestion.value;
@@ -15,7 +14,6 @@ function SendQuestion() {
   fetch("https://api.openai.com/v1/completions", {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
       Authorization: "Bearer " + OPENAI_API_KEY,
     },
